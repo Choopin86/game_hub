7eef4464ace602981471c549f29896ad9b80ba1f
Ledjan Miçani <ledjanmicani@gmail.com>
2024-09-03 20:10:48 -0400

    Create a custom hook to fetch the games

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,19 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`
      }}>
+
         <GridItem area="nav">
             <NavBar />
         </GridItem>
+
         <Show above="lg">
             <GridItem area="aside" >Aside</GridItem>
         </Show>
+
         <GridItem area="main" >
             <GameGrid />
         </GridItem>
+        
      </Grid>
     )   
 }
